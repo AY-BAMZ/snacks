4933b8048901179cd6055de810d944e601ce0f71
AY-BAMZ <aybamz181@gmail.com>
2020-10-16 11:28:03 +0100

    Updates

---
+++ precache-manifest.509dbee56f5ed5b191653a73397c21e0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b9799bf7807c78274a481293d832f8",
+    "url": "/AY-BAMZ/snacks.git/index.html"
+  },
+  {
+    "revision": "1414bd6dd1443d196c85",
+    "url": "/AY-BAMZ/snacks.git/static/css/2.2fc483f1.chunk.css"
+  },
+  {
+    "revision": "11ae777cce74cf873986",
+    "url": "/AY-BAMZ/snacks.git/static/css/main.f3f6666c.chunk.css"
+  },
+  {
+    "revision": "1414bd6dd1443d196c85",
+    "url": "/AY-BAMZ/snacks.git/static/js/2.8177a7c2.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/AY-BAMZ/snacks.git/static/js/2.8177a7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ae777cce74cf873986",
+    "url": "/AY-BAMZ/snacks.git/static/js/main.a4d83907.chunk.js"
+  },
+  {
+    "revision": "752a2e96b31a4b46509f",
+    "url": "/AY-BAMZ/snacks.git/static/js/runtime-main.5e6cdc38.js"
+  },
+  {
+    "revision": "17a094a373575f073504b07a07ec590c",
+    "url": "/AY-BAMZ/snacks.git/static/media/facebook.17a094a3.svg"
+  },
+  {
+    "revision": "2371f4293673e26527d53c626b832912",
+    "url": "/AY-BAMZ/snacks.git/static/media/instagram.2371f429.svg"
+  },
+  {
+    "revision": "609f7bf42f11802b923234fa58ca5c5b",
+    "url": "/AY-BAMZ/snacks.git/static/media/logo.609f7bf4.svg"
+  },
+  {
+    "revision": "4c47721daf8c312effe677be39b720da",
+    "url": "/AY-BAMZ/snacks.git/static/media/twitter.4c47721d.svg"
+  },
+  {
+    "revision": "36d3147deabb995fb114b88f9bf220b7",
+    "url": "/AY-BAMZ/snacks.git/static/media/whatsapp.36d3147d.svg"
+  }
+]);
